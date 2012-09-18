e49d531da495c8ef3b7c09b9e4eb2449d5ac40c9
ndeverge <ndeverge@ekito.fr>
2012-09-18 15:39:43 +0200

    A first message producer

--- app/bootstrap/Global.java
+++ app/bootstrap/Global.java
@@ -1,3 +1,5 @@
+package bootstrap;
+
 import org.apache.activemq.xbean.XBeanBrokerService;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -8,11 +10,17 @@ import play.Play;
 
 public class Global extends GlobalSettings {
 
-	private GenericXmlApplicationContext springContext;
+	private static GenericXmlApplicationContext springContext;
 
 	@Override
 	public void onStart(final Application app) {
 
+		loadSpringContext();
+
+		super.onStart(app);
+	}
+
+	protected void loadSpringContext() {
 		springContext = new GenericXmlApplicationContext();
 
 		String context = new String();
@@ -26,26 +34,32 @@ public class Global extends GlobalSettings {
 		// start the Spring context
 		springContext.load(context);
 		springContext.refresh();
-
-		super.onStart(app);
 	}
 
 	@Override
 	public void onStop(final Application app) {
-		// stop the embedded broker
-		XBeanBrokerService brokerService = springContext
-				.getBean(XBeanBrokerService.class);
-
-		try {
-			brokerService.stop();
-		} catch (Exception e) {
-			Logger.error("Error stopping the JMS broker", e);
-		}
-
-		// close the spring context
-		springContext.close();
+		destroySpringContext();
 
 		super.onStop(app);
 	}
 
+	protected void destroySpringContext() {
+		XBeanBrokerService brokerService = springContext
+				.getBean(XBeanBrokerService.class);
+
+		if (brokerService != null) {
+			try {
+				brokerService.stop();
+			} catch (Exception e) {
+				Logger.error("Error stopping the JMS broker", e);
+			}
+		}
+
+		// close the spring context
+		springContext.close();
+	}
+
+	public static <T> T getBean(final Class<T> clazz) {
+		return springContext.getBean(clazz);
+	}
 }
