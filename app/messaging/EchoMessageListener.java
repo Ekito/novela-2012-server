c2d064c562e1fa0e3771c5aa9c1a4cad2aaf70b4
ndeverge <ndeverge@ekito.fr>
2012-09-21 16:24:09 +0200

    Kaazing integration with Play app

--- app/messaging/EchoMessageListener.java
+++ app/messaging/EchoMessageListener.java
@@ -1,7 +1,9 @@
 package messaging;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
 
 import play.Logger;
 
@@ -10,6 +12,16 @@ public class EchoMessageListener implements MessageListener {
 	@Override
 	public void onMessage(final Message message) {
 		Logger.info("Received a message");
+
+		if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			try {
+				Logger.info("Message is '" + textMessage.getText() + "'");
+			} catch (JMSException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 
 	}
 
--- app/messaging/LocationProducer.java
+++ app/messaging/LocationProducer.java
@@ -1,76 +1,43 @@
 package messaging;
 
-import java.util.Properties;
-
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
+import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.Topic;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 
 import models.Location;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
+
+import play.Logger;
+import play.libs.Json;
 
 @Component
 public class LocationProducer {
 
-	// @Autowired
-	// private JmsTemplate template;
-	//
-	// @Autowired
-	// private Topic locationTopic;
-	//
-	// public void publishLocation(final Location aLocation) {
-	//
-	// template.send(locationTopic, new MessageCreator() {
-	//
-	// @Override
-	// public Message createMessage(final Session session)
-	// throws JMSException {
-	//
-	// Logger.debug("Sending a location Message !");
-	//
-	// return session.createObjectMessage(aLocation);
-	// }
-	// });
-	// }
+	@Autowired
+	private JmsTemplate template;
 
-	private MessageProducer producer;
-
-	private Session session;
-
-	public LocationProducer() throws NamingException, JMSException {
-
-		Properties props = new Properties();
-		props.put(InitialContext.INITIAL_CONTEXT_FACTORY,
-				"com.kaazing.gateway.jms.client.stomp.StompInitialContextFactory");
-
-		props.put(Context.PROVIDER_URL,
-				"ws://ec2-46-137-8-173.eu-west-1.compute.amazonaws.com:8001/jms");
-		InitialContext ctx = new InitialContext(props);
-		ConnectionFactory connectionFactory = (ConnectionFactory) ctx
-				.lookup("ConnectionFactory");
-
-		Connection connection = connectionFactory.createConnection(null, null);
-
-		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-		Topic topic = (Topic) ctx.lookup("/topic/LocationTopic");
-
-		producer = session.createProducer(topic);
-
-	}
+	@Autowired
+	private Topic locationTopic;
 
 	public void publishLocation(final Location aLocation) {
-		try {
-			producer.send(session.createTextMessage("Hello World"));
-		} catch (JMSException e) {
-			e.printStackTrace();
-		}
+
+		template.send(locationTopic, new MessageCreator() {
+
+			@Override
+			public Message createMessage(final Session session)
+					throws JMSException {
+
+				String asText = Json.stringify(Json.toJson(aLocation));
+
+				Logger.debug("Sending a location message: " + asText);
+
+				return session.createTextMessage(asText);
+			}
+		});
 	}
 }
