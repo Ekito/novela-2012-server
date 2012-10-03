4a1f4abffdb0954ffa363c9aea91c7a7326ee05a
Romain Piel <romain.piel@gmail.com>
2012-10-03 11:22:56 +0200

    about page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.map;
+import views.html.about;
 
 public class Application extends Controller {
 
@@ -12,5 +13,9 @@ public class Application extends Controller {
 
 	public static Result fullScreenMap() {
 		return ok(map.render());
+	}
+
+	public static Result about() {
+		return ok(about.render());
 	}
 }
