77e368dad166cdcbfb6bd60d6dd97daf67d3fe93
Arnaud GIULIANI <agiuliani@ekito.fr>
2012-10-16 11:08:55 +0200

    fixing filtering

--- app/util/LocationFilter.scala
+++ app/util/LocationFilter.scala
@@ -9,13 +9,17 @@ object LocationFilter {
 
     def tooCloseLocations(l1: Location, l2: Location): Boolean = {
       def d = math.acos(math.sin(l1.getLat()) * math.sin(l2.getLat()) + math.cos(l1.getLat()) * math.cos(l2.getLat() * math.cos(l1.getLon() - l2.getLon()))) * 6366
-      d < delta && (!l1.isStart || !l2.isStart)
+      def r = d < delta && !(l1.isStart || l2.isStart)
+      if (l1.isStart || l2.isStart){
+    	  println("r="+r+" l1.isStart =" + l1.isStart + " l2.isStart =" + l2.isStart)
+      }
+      r
     }
 
     def filterNearLocationsRec(loc: List[Location], result: List[Location]): List[Location] = {
       if (loc.isEmpty) result
       else if (!result.isEmpty && tooCloseLocations(loc.head, result.head)) filterNearLocationsRec(loc.tail, result)
-      else filterNearLocationsRec(loc.tail, loc.head::result)
+      else filterNearLocationsRec(loc.tail, loc.head :: result)
     }
 
     filterNearLocationsRec(locations.toList, List())
